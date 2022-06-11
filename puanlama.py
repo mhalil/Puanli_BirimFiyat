72d356b544a65782a679413ce63edfb25b335b27
Mustafa Halil <halil.mustafa@gmail.com>
2022-06-11 17:36:07 +0300

    "kontrol" fonk. eklendi

--- puanlama.py
+++ puanlama.py
@@ -1,9 +1,12 @@
 # Birim Fiyat Teklif Usulü Yapılan İhalelerde, Puanlama Sistemine uygun birim fiyat belirlemeye dair kod çalışması. Her bir poz için, İdarece belirlenen oranlara uygun teklif birim fiyat tespit eden python uygulaması.
 
-bf = [1, 1, 1, 1, 1]            # Birim fiyatlar. Bu değer değişecek
+from turtle import Turtle
+
+
+bf = [28, 5, 17, 40, 11]            # Birim fiyatlar. Bu değer değişecek 28, 5, 17, 40, 11 - 1, 1, 1, 1, 1
 mkt = [174, 420, 384, 128, 174] # Miktarlar. Bu değer sabit kalacak
 
-toplam_tutarlar = []            # Birim fiyat ile miktar çarpımından oluşan değerlerin tutulduğu liste
+toplam_tutarlar = []            # Birim fiyat ile miktar çarpımından oluşan TOPLAM TUTARLARI barıdıran liste
 
 alt_yuzde = [0.21, 0.09, 0.30, 0.23, 0.08] # min. yüzdelik dilim
 ust_yuzde = [0.25, 0.12, 0.33, 0.26, 0.10] # max. yüzdelik dilim
@@ -14,10 +17,10 @@ max_tutarlar = []                  # İş kalemlerinin en yüksek (MAX) toplam t
 def tutar():                    # Teklif Birim fiyatlar ile Miktarların çarpımını barındıran liste.
     toplam_tutarlar.clear()
     for i in range(len(bf)):
-        toplam_tutarlar.append(bf[i] * mkt[i])
+        toplam_tutarlar.append(round(bf[i] * mkt[i],2))
     return toplam_tutarlar
 
-tutar()
+print("toplam_tutarlar: ", tutar())
 
 def genel_toplam ():            # GENEL TOPLAMI ( Teklif Tutarı ) Hesaplayan fonksiyonu
     return sum(toplam_tutarlar)
@@ -27,15 +30,25 @@ genel_toplam()
 def min_tutar():                # İş kalemlerinin en düşük (MİN) toplam tutarlarını hesaplayan fonksiyon
     min_tutarlar.clear()
     for i in range(len(alt_yuzde)):
-        min_tutarlar.append(alt_yuzde[i] * genel_toplam())
+        min_tutarlar.append(round(alt_yuzde[i] * genel_toplam(),2))
     return min_tutarlar
 
-print("min. tutarlar:", min_tutar())
+print("min_tutarlar:", min_tutar())
 
 def max_tutar():                # İş kalemlerinin en yüksek (MAX) toplam tutarlarını  hesaplayan fonksiyon
     max_tutarlar.clear()
     for i in range(len(ust_yuzde)):
-        max_tutarlar.append(ust_yuzde[i] * genel_toplam())
+        max_tutarlar.append(round(ust_yuzde[i] * genel_toplam(),2))
     return max_tutarlar
 
-print("max. tutarlar:", max_tutar())
+print("max_tutarlar:", max_tutar())
+
+def kontrol(tutar, min, max):
+    if (tutar >= min) and (tutar <= max):
+        print("İşlem Başarılı")
+        return True
+    
+    else:
+        print("hata")
+
+kontrol(toplam_tutarlar, min_tutarlar, max_tutarlar)
